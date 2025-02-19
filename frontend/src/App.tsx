adb7340ed4598f73ba7f2f8b5e6635b52bcdeab4
Sai Dushyant <126763864+SaiDushyant@users.noreply.github.com>
2025-02-19 19:50:50 +0530

    Cleaned frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,7 @@
-// import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
-// import TestBG from "./TestBG";
-// Assume you have a Home component for your main page
-// import Home from "./Home";
-
 import FlashcardGenerator from "./FlashcardGenerator";
 
 function App() {
-  return (
-    <FlashcardGenerator />
-
-    // <Router>
-    //   <nav className="p-4 bg-gray-100">
-    //     <Link to="/" className="mr-4 text-blue-600 hover:underline">
-    //       Home
-    //     </Link>
-    //     <Link to="/testbg" className="text-blue-600 hover:underline">
-    //       Test BG
-    //     </Link>
-    //   </nav>
-    //   <Routes>
-    //     <Route path="/" element={<Home />} />
-    //     <Route path="/testbg" element={<TestBG />} />
-    //   </Routes>
-    // </Router>
-  );
+  return <FlashcardGenerator />;
 }
 
 export default App;
